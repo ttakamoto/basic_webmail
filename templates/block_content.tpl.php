aaa0e6456c5f091322f9c03e11e851a092cc90f4
ttakamoto <tiago.takamoto@gmail.com>
2013-11-22 23:18:11 -0200

    Implementing filtering block messages according to profile viewer
    If user is visualizing a profile page, it get the profile email address
    and filter the block messages according to this e-mail address.

--- templates/block_content.tpl.php
+++ templates/block_content.tpl.php
@@ -1,6 +1,8 @@
 <?php
 /* arrays structure received
  * 
+ * filtering = 0 or 1
+ * from = string
  * inbox = array(
  *   [0] = array(
  *     'message_subject' = string,
@@ -37,16 +39,29 @@
     display:inline;
   }
 </style>  
+<?php if ($is_filtering == 1): ?>
+<p><b>Filter:</b> <?php print $from; ?></p>
+<?php else: ?>
+<p><b>Filter:</b> All</p>
+<?php endif; ?>
 <p><h3>Inbox</h3></p>
 <ul>
-  <?php foreach($inbox as $msg): ?>
-  <li><b><?php print $msg['message_date'];?></b> | <span alt='<?php print $msg['from_address'];?>'><?php print $msg['message_subject'];?></span></li>
-  <?php endforeach; ?>
+  <?php if (count($inbox) > 0): ?>
+    <?php foreach($inbox as $msg): ?>
+    <li><b><?php print $msg['message_date'];?></b> | <span alt='<?php print $msg['from_address'];?>'><?php print $msg['message_subject'];?></span></li>
+    <?php endforeach; ?>
+  <?php else: ?>
+    <li><i>&lt empty &gt;</i></li>
+  <?php endif; ?>
 </ul>
 
 <p><h3>Sent</h3></p>
 <ul>
-  <?php foreach($sent as $msg): ?>
-  <li><b><?php print $msg['message_date'];?></b> | <span alt='<?php print $msg['from_address'];?>'><?php print $msg['message_subject'];?></span></li>
-  <?php endforeach; ?>
+  <?php if (count($sent) > 0): ?>
+    <?php foreach($sent as $msg): ?>
+    <li><b><?php print $msg['message_date'];?></b> | <span alt='<?php print $msg['from_address'];?>'><?php print $msg['message_subject'];?></span></li>
+    <?php endforeach; ?>
+  <?php else: ?>
+    <li><i>&lt empty &gt;</i></li>
+  <?php endif; ?>
 </ul>
